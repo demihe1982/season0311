d86712b8285089d79cddae6e17bf4853e0b4c9a7
edayahe <demi.he@ericsson.com>
2017-12-05 18:28:42 +0800

    add code

---
+++ src/main/java/cn/dyan/App.java
@@ -0,0 +1,13 @@
+package cn.dyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args){
+        SpringApplication.run(App.class,args);
+    }
+}
